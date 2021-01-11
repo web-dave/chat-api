43d120a4d3f7fe2a21e7b564c45225b23f98fa1d
Dave <dave@webdave.de>
2021-01-11 19:09:44 +0100

    express

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const WebSocket = require("ws");
 const { v4: getID } = require("uuid");
+const PORT = process.env.PORT || 3000;
+const INDEX = "/index.html";
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const clients = [];
 const room = {
